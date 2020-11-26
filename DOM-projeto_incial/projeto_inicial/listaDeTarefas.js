39ff348637a12ebd09ac6a7727f7906ef85b1de6
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2020-11-26 13:34:05 -0300

    Aula 4 - inclusão do botão concluir e suas finalidades

--- DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
+++ DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
@@ -12,10 +12,26 @@ const criarTarefa = (evento) => {
     
     tarefa.innerHTML = conteudo;
 
-    lista.insertBefore(tarefa, valor);
+    tarefa.appendChild(BotaoConclui());//inclui o botão (função Botao conclui) dentro da li (tarefa)
+    lista.appendChild(tarefa);
     input.value = " ";
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
-
 novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');//cria botão
+    botaoConclui.classList.add('check-button');//adiciona a classe para o botão
+    botaoConclui.innerText = 'concluir';//adiciona o texto no botão
+    botaoConclui.addEventListener('click', concluirTarefa)//escuta o botão concluir, quando clicado dispara a função
+  
+    return botaoConclui;
+}
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target;//para saber qual 
+    const tarefaCompleta = botaoConclui.parentElement;
+
+    tarefaCompleta.classList.toggle('done');
+}
